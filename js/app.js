32d6d3d540d6ec12a56f7ea2db839bacae79a3bc
Zayah117 <isaiah.baker@simpledove.com>
2016-06-27 12:07:07 -0400

    Make change to way enemy moves

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.loc += (1 * dt);
+    this.loc += (this.speed * dt);
+    // TODO handle collision with player
 };
 
 // Draw the enemy on the screen, required method for game
